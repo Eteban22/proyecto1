1e8ee7e6197bc93bf54b2e6fa956203d01c4fb12
Esteban Mendieta <esteban_mendieta_1989@hotmail.com>
2023-06-30 18:28:20 -0300

    Sexto commit - Actualización de DB, EndPoint y Manager

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ const bodyParser = require(`body-parser`);
 const app = express();
 
 const { guardarFrutas, leerFrutas } = require("./src/database/electro.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    DB = leerFrutas();
+    next();
+});
+
+
+//Manejo de errores simple (TP)
+app.get('*', (req, res) => {
+    res.status(404).send(`Lo siento, la página que buscas no existe.`);
+});
+
+//Ejecución del servidor
+app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(`Ejecutandose en http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/frutas`);
+});
