579477e3de48f8ff53da8f84890dd8c8c9fb15ec
Esteban Mendieta <esteban_mendieta_1989@hotmail.com>
2023-06-30 23:15:15 -0300

    Sextimo commit - Creación de METODO GET y POST

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 const express = require("express");
+const { guardarElectrodomestico, leerElectrodomestico, findOneById } = require("./src/electro.manager.js");
 
-const dotenv = require("dotenv");
-dotenv.config();
+require('dotenv').config();
 
 const bodyParser = require(`body-parser`);
 const app = express();
 
-const { guardarFrutas, leerFrutas } = require("./src/database/electro.json");
-
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    DB = leerFrutas();
-    next();
+// Obtener todos los Electrodomesticos.
+app.get('/electro', (req, res) => {
+    leerElectrodomestico()
+        .then((electrodomesticos) => res.status(200).send(electrodomesticos))
+        .catch((error) => res.status(400).send(error.message));
 });
 
-//Manejo de errores simple (TP)
+// Obtener un Electrodomestico específico.
+app.get('/electro/:id', (req, res) => {
+    const { id } = req.params;
+
+    findOneById(Number(id))
+        .then((coche) => res.status(200).send(coche))
+        .catch((error) => res.status(400).send(error.message));
+});
+
+// Crear un nuevo Electrodomesticos.
+app.post('/electro', (req, res) => {
+    const { prodType, brand, model, price, stock } = req.body;
+    //Variables para almacenar en INT
+    const intPrice = Number(price);
+    const intStock = Number(stock);
+
+    guardarElectrodomestico({ prodType, brand, model, price: intPrice, stock: intStock })
+        .then((electrodomesticos) => res.status(201).send(electrodomesticos))
+        .catch((error) => res.status(400).send(error.message));
+});
+
+//Manejo de errores simple
 app.get('*', (req, res) => {
-    res.status(404).send(`Lo siento, la página que buscas no existe.`);
+    res.status(404).send(`<h1>Error 404</h1><h2>Lo siento, la página ingresada no existe.</h2><h3>Intente nuevamente.</h3>`);
 });
 
-//Ejecución del servidor
+//Escucha de peteciones
 app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
-    console.log(`Ejecutandose en http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/frutas`);
+    console.log(`Ejecutandose en http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/electro`);
 });
--- src/electro.manager.js
+++ src/electro.manager.js
@@ -1,1 +1,67 @@
 const fs = require("fs");
+const path = require("path");
+
+const DB = path.join(__dirname, "./database/electro.json");
+
+function create(frutas) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(DB, JSON.stringify(frutas, null, "\t"), "utf8", (error) => {
+            if (error) reject(new Error("Error. No se pudo escribir"));
+
+            resolve(true);
+        });
+    });
+}
+
+function leerDB() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(DB, "utf8", (error, contenido) => {
+            if (error) reject(new Error("Error. No se pudo leer"));
+
+            resolve(JSON.parse(contenido));
+        });
+    });
+}
+
+function generarId(electrodomesticos) {
+    let mayorId = 0;
+
+    electrodomesticos.forEach((electrodomestico) => {
+        if (Number(electrodomestico.id) > mayorId) {
+            mayorId = Number(electrodomestico.id);
+        }
+    });
+
+    return mayorId + 1;
+}
+
+async function findOneById(id) {
+    if (!id) throw new Error("Error. El Id está indefinido.");
+
+    const electrodomesticos = await leerDB();
+    const electrodomestico = electrodomesticos.find((element) => element.id === id);
+
+    if (!electrodomestico) throw new Error("Error. El Id no corresponde a un coche en existencia.");
+
+    return electrodomestico;
+}
+
+async function guardarElectrodomestico(electrodomestico) {
+    // eslint-disable-next-line max-len
+    if (!electrodomestico?.prodType || !electrodomestico?.brand || !electrodomestico?.model || !electrodomestico?.price || !electrodomestico?.stock) throw new Error("Error. Datos incompletos.");
+
+    let electrodomesticos = await leerDB();
+    const electrodomesticoConId = { id: generarId(electrodomesticos), ...electrodomestico };
+
+    electrodomesticos.push(electrodomesticoConId);
+    await create(electrodomesticos);
+
+    return electrodomesticoConId;
+}
+
+async function leerElectrodomestico() {
+    const electros = await leerDB();
+    return electros;
+}
+
+module.exports = { guardarElectrodomestico, leerElectrodomestico, findOneById };
